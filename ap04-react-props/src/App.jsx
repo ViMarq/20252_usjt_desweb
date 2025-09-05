3aa8dcfc3381e9cdca65055a311cce0d6b503359
Vitória Marquini Oliveira <822161472@ulife.com.br>
2025-09-04 21:51:24 -0300

    learn[ap04-react-props]: aprender container

--- ap04-react-props/src/App.jsx
+++ ap04-react-props/src/App.jsx
@@ -1,7 +1,34 @@
 const App = () => {
   return(
-    <div className="container-fluid bg-primary p-5">
-      {/* <i className="fa-hippo fa-2x fa-solid"></i> */}
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <i className="fa-hippo fa-2x fa-solid"></i>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <div className="card">
+            <div className="card-header text-muted">
+              03/09/2025
+            </div>
+          <div className="card-body d-flex">
+            <i className="fa-solid fa-2x fa-car"></i>
+          </div>
+            <p>Carro</p>
+            <p>Porche amarelo</p>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+          
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+          
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+          
+        </div>
+      </div>
     </div>
   )
 }
