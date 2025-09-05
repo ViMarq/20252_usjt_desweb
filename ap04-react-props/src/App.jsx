f4cf9a767e503ea363c9bb7fdb456d33c83fc675
Vitória Marquini Oliveira <822161472@ulife.com.br>
2025-09-04 21:05:11 -0300

    learn[ap04-react]: iniciar react props

---
+++ ap04-react-props/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return(
+    <div className="container-fluid bg-primary p-5">
+      {/* <i className="fa-hippo fa-2x fa-solid"></i> */}
+    </div>
+  )
+}
+
+export default App
