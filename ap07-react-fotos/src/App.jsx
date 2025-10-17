f3a403940ba0b901a2a5b9fc0e9b5c14ef38b619
Vitoria <vimarquini@gmail.com>
2025-10-17 08:33:45 -0300

    learn(ap-07-react): implementa variavel de estado
    - modificou app de arrow function para classe
    - adicionou varialvel de estado para Busca
    - instalou biblioteca pexels

--- ap07-react-fotos/src/App.jsx
+++ ap07-react-fotos/src/App.jsx
@@ -1,16 +1,46 @@
 import {Button} from 'primereact/button'
 import Busca from './componentes/Busca'
+import React from 'react'
+import { createClient } from 'pexels'
 
-const App = () => {
-  return(
-    <div>
-      <div className='flex mb-3'>
-        <i className='pi pi-car mr-3'></i>
-        <i className='pi pi-car'></i>
+class App extends React.Component{
+  //somente componentes de classe podem ter estado, por isso mudamos de arrow function para classe
+  
+  chavePexels = 'VMhQbYzyGQXJIscWTojLkeKEY0jMuEKWDoLlH0u19OpTOogw1XtVt7TE'
+
+  pexelsClient = null
+
+  componentDidMount(){
+    this.pexelsClient = createClient(this.chavePexels)
+  }
+
+  onBuscaRealizada = (termo) => {
+    this.pexelsClient.photos.search({
+      query: termo
+    })
+    .then((result) => console.log(result))
+  }
+
+  render(){
+    return(
+      <div className= 'grid border-1 justify-content-center border-400 border-round'>
+        <div className="col-12">
+          <div className='flex mb-3'>
+            <i className='pi pi-car mr-3'></i>
+            <i className='pi pi-car'></i>
+          </div>
+        </div>
+        <div className="col-12">
+          <h1>Exibir uma lista de ...</h1>
+        </div>
+        <div className="col-8">
+          <Busca onBuscaRealizada={this.onBuscaRealizada} />
+  
+        </div>
       </div>
-        <Busca />
-    </div>
-  )
+    )
+
+  }
 }
 
 export default App
--- ap07-react-fotos/src/componentes/Busca.jsx
+++ ap07-react-fotos/src/componentes/Busca.jsx
@@ -5,25 +5,51 @@ import { InputIcon } from 'primereact/InputIcon'
 import { InputText } from 'primereact/inputtext'
 export default class Busca extends Component {
   state = {
-// uma variável de estado tem a característica de quando você altera o valor dela, a interface é atualizada automaticamente
+// uma variável de estado tem a característica de que quando você altera o valor dela, a interface é atualizada automaticamente
     termoDeBusca: ''    
-  }  
+  }
+  
+  onTermoAlterado = (event) => {
+    //atualizar o valor de busca na variável de estado
+    // console.log(event.target.value)
+    // this.state.termoDeBusca = event.target.value
+    this.setState({
+      termoDeBusca: event.target.value
+    })
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    //chamar a função passada via props
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+  }
+
+  
+
   render() {
     return (
-      <div className='flex flex-column'>
-        <IconField
-            className='w-full'
-            iconPosition='left'>
-            <InputIcon className='pi pi-search' />
-            <InputText
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column'>
+          <IconField
+              className='w-full'
+              iconPosition='left'>
+              <InputIcon className='pi pi-search' />
+              <InputText
+                value={this.state.termoDeBusca}
+                onChange={this.onTermoAlterado}
                 className='w-full'
-                placeholder='Buscar' />
-        </IconField>
-        <Button
-         className='mt-3'
-         label='OK'
-         />
-      </div>
+                placeholder={this.props.dica} />
+          </IconField>
+          <Button
+          className='mt-3'
+          label='OK'
+          />
+        </div> 
+      </form>
     )
   }
+}
+
+Busca.defaultProps = {
+  dica: 'Buscar...'
 }
