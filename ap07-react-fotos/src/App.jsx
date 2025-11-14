0658bba081053fecbda21ec6363c537dade77e22
Vitoria <vimarquini@gmail.com>
2025-11-13 21:48:30 -0300

    learn(ap-07-react-fotos): implementar backend
    - Criar banco de dados para salvar logs de busca de fotos

--- ap07-react-fotos/src/App.jsx
+++ ap07-react-fotos/src/App.jsx
@@ -1,41 +1,38 @@
-import {Button} from 'primereact/button'
+import { Button } from 'primereact/button'
 import Busca from './componentes/Busca'
 import React from 'react'
-import { createClient } from 'pexels'
+import axios from 'axios'
 import ListaImagens from './componentes/ListaImagens'
 
-class App extends React.Component{
+class App extends React.Component {
   //somente componentes de classe podem ter estado, por isso mudamos de arrow function para classe
-  
-state = {
-  photos: []
-}
 
-  chavePexels = 'VMhQbYzyGQXJIscWTojLkeKEY0jMuEKWDoLlH0u19OpTOogw1XtVt7TE'
-
-  pexelsClient = null
-
-  componentDidMount(){
-    this.pexelsClient = createClient(this.chavePexels)
+  state = {
+    photos: []
   }
 
   onBuscaRealizada = (termo) => {
-    this.pexelsClient.photos.search({
-      query: termo
+    const httpClient = axios.create({
+      baseURL: 'http://localhost:3000/'
     })
-    .then((result) => {
-      //não faça assim
-      //this.state.photos = result.photos
-      //faça assim
-      this.setState({
-        photos: result.photos
+    httpClient.get('/search', {
+      params: {
+        query: termo
+      }
+    })
+      .then(({ data }) => {
+        this.setState({
+          photos: data.photos
+        })
       })
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
-  render(){
-    return(
-      <div className= 'grid border-1 justify-content-center border-400 border-round'>
+  render() {
+    return (
+      <div className='grid border-1 justify-content-center border-400 border-round'>
         <div className="col-12">
           <div className='flex mb-3'>
             <i className='pi pi-car mr-3'></i>
@@ -52,7 +49,7 @@ state = {
           <div className='grid'>
             <ListaImagens
               imageStyle='col-12 md:col-6 lg:col-4 xl:col-3'
-              photos={this.state.photos}/>
+              photos={this.state.photos} />
           </div>
         </div>
       </div>
